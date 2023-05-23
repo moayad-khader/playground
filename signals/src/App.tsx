dd252d08d29466db9026a80c464d69e28af572c2
moayad-khader <moayadkhader99@gmail.com>
2023-05-24 00:33:09 +0300

    upd

---
+++ signals/src/App.tsx
@@ -0,0 +1,51 @@
+ /** @jsxImportSource jotai-signal */
+
+import { atom } from 'jotai/vanilla';
+import { useAtom, useSetAtom } from 'jotai/react';
+import { $ } from 'jotai-signal';
+
+const countAtom = atom(0);
+
+const CounterWithSignal = () => {
+  return (
+    <div>
+      <h1>With $(atom)</h1>
+      Count: {$(countAtom)} ({Math.random()})
+    </div>
+  );
+};
+
+const Counter = () => {
+  const [count] = useAtom(countAtom);
+  return (
+    <div>
+      <h1>With useAtom(atom)</h1>
+      Count: {count} ({Math.random()})
+    </div>
+  );
+};
+
+const Controls = () => {
+ 
+  const setCount = useSetAtom(countAtom);
+  console.log("sss")
+  return (
+    <div>
+      <button type="button" onClick={() => setCount((c) => c + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return(
+    <>
+    <Controls />
+    <CounterWithSignal />
+    <Counter />
+  </>
+  )
+};
+
+export default App;
